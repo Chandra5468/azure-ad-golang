c5f27ac027b03cf0c63babe05eae0b4d1e6cbb38
Chandra_Ageer <ageer_chandar@blackbelthelp.com>
2025-03-06 02:39:46 -0500

    mongo function to fetch configuration details added

--- models/mango/tenants/azureconfig.model.go
+++ models/mango/tenants/azureconfig.model.go
@@ -1,1 +1,44 @@
 package tenants
+
+import (
+	"context"
+	"os"
+
+	"github.com/Chandra5468/azure-ad-golang/models/mango"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Products struct {
+	TenantId string       `bson:"tenantId"`
+	Products ProductsList `bson:"products"`
+}
+type ProductsList struct {
+	Ipaas                IpaasProduct                `bson:"banneripass"`
+	AzureActiveDirectory AzureActiveDirectoryProduct `bson:"azureActiveDirectory"`
+}
+
+type IpaasProduct struct {
+	Type string `bson:"type"`
+	URL  string `bson:"url"`
+}
+
+type AzureActiveDirectoryProduct struct {
+	GrantType    string `bson:"grant_type"`
+	ClientId     string `bson:"client_id"`
+	ClientSecret string `bson:"client_secret"`
+	UserName     string `bson:"username"`
+	Password     string `bson:"password"`
+	Resource     string `bson:"resource"`
+	Scope        string `bson:"scope"`
+	URL          string `bson:"url"`
+}
+
+func GetAzureConfigs(tenantId string, ctx context.Context) (*Products, error) {
+	var pds Products
+	err := mango.MongoClient.Database(os.Getenv("DEFAULT_DB")).Collection("products").FindOne(ctx, bson.M{"tenantId": "BBH_" + tenantId}).Decode(&pds)
+	if err != nil {
+		return nil, err
+	} else {
+		return &pds, err
+	}
+}
